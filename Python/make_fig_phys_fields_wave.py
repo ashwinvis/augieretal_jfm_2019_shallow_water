3e1e5bbf7785d45ed7e14ef0c5525e48ed2916e8
Ashwin Vishnu <avmo@kth.se>
2018-09-25 17:33:56 +0200

    Increase font size phys fields

--- Python/make_fig_phys_fields_wave.py
+++ Python/make_fig_phys_fields_wave.py
@@ -47,7 +47,7 @@ def fig_phys_subplot(sim, fig, ax, key_field, x_slice=None, y_slice=None, cmap='
     cbar.solids.set_rasterized(True)
     
 if __name__ == '__main__':
-    matplotlib_rc()
+    matplotlib_rc(fontsize=9)
     path_fig = exit_if_figure_exists(__file__, extension='.pdf')
     set_figsize(6.65, 6)
     fig, axes = plt.subplots(2, 2)
