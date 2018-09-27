6b3530aa180b70fbfc71f8d129d988a7a99ba6b7
Ashwin Vishnu <avmo@kth.se>
2018-09-27 05:33:16 +0200

    Flatness

--- Python/make_fig_flatness.py
+++ Python/make_fig_flatness.py
@@ -3,14 +3,14 @@ import pylab as pl
 import fluidsim as fls
 
 from base import _k_f, set_figsize, _rxs_str_func, matplotlib_rc
-from paths import paths_sim, exit_if_figure_exists
+from paths import paths_sim, exit_if_figure_exists, load_df
 
 
-fontsize = 7
+fontsize = 9
 fontsize_inset = fontsize - 2
 
 
-def _ax_inset(fig, xlabel, left, bottom=0.6, width=0.35 / 2, height=0.45 / 2):
+def _ax_inset(fig, xlabel, left, bottom=0.6, width=0.35 / 2, height=0.45 / 4):
     ax_inset = fig.add_axes([left, bottom, width, height])
     ax_inset.set_xlabel(xlabel, size=fontsize_inset)
     ax_inset.set_ylabel('$F_T / F_L$', size=fontsize_inset)
@@ -25,15 +25,15 @@ def _ax_inset(fig, xlabel, left, bottom=0.6, width=0.35 / 2, height=0.45 / 2):
     return ax_inset
 
 
-def fig12_flatness(path, fig, ax, tmin=0, tmax=1000, delta_t=0.5):
+def fig12_flatness(path, fig, ax, tmin=0, tmax=1000, delta_t=0.5, 
+                   key_var=('uy', 'ux'), run_nb=0, ax_inset=None, cache=False):
     sim = fls.load_sim_for_plot(path, merge_missing_params=True)
     order = [2, 4]
-    key_var = ['uy', 'ux']
     rxs, So_var_dict, deltax = _rxs_str_func(
-        sim, order, tmin, tmax, delta_t, key_var)
+        sim, order, tmin, tmax, delta_t, key_var, cache=cache)
 
     ax.set_xlabel('$r_x/L_f$')
-    ax.set_ylabel('$F_T, F_L$')
+    # ax.set_ylabel('$F_T, F_L$')
 
     # ax.set_title('Flatness of longitundinal and transverse increments')
     # ax.hold(True)
@@ -43,47 +43,78 @@ def fig12_flatness(path, fig, ax, tmin=0, tmax=1000, delta_t=0.5):
     _label = {'ux': '$F_L$', 'uy': '$F_T$'}
     L_f = pl.pi / _k_f(sim.params)
     color_list = ['r', 'b', 'g', 'c', 'm', 'r', 'b']
-    cond = pl.logical_and(rxs > 0.012 * L_f, rxs < 0.06 * L_f)
-    F = {}
-    for i, key in enumerate(key_var):
-        color1 = color_list[i]
+    def get_F(key):
         So_4 = So_var_dict['{0}_{1:.0f}'.format(key, 4)]
         So_2 = So_var_dict['{0}_{1:.0f}'.format(key, 2)]
-        F[key] = So_4 / So_2 ** 2
-        ax.plot(rxs / L_f, F[key], color1,
+        return So_4 / So_2 ** 2
+
+    for key in key_var:
+        color1 = color_list[run_nb] 
+        ax.plot(rxs / L_f, get_F(key), color1,
                 linewidth=1, label=_label[key])
 
-    ax.set_xlim([2e-3, 10])
+    ax.set_xlim([None, 10])
     ax.set_ylim([2, 500])
-    ax.plot(rxs[cond] / L_f, 1 / rxs[cond], 'k', linewidth=0.5)
-    x_text = rxs[cond].mean() / L_f
-    y_text = 1. / rxs[cond].mean() * 1.05
-    ax.text(x_text, y_text, '$r^{-1}$')
-    ax_inset = _ax_inset(fig, '$r_x/L_f$', 0.25 + 0.025)
-    ax_inset.semilogx(rxs / L_f, F['uy'] / F['ux'], 'k',
-                      linewidth=1)
 
-    ax.legend(fontsize=fontsize, loc='lower left')
+    # r^-1 line
+    cond = pl.logical_and(rxs > 0.012 * L_f, rxs < 0.06 * L_f)
+    fac = 10
+    ax.plot(rxs[cond] / L_f, fac / rxs[cond], 'k', linewidth=0.5)
+    x_text = rxs[cond].min() * 3 / fac
+    y_text = fac / rxs[cond].min()
+    ax.text(x_text, y_text, '$r_x^{-1}$')
 
+    if ax_inset is not None:
+        ax_inset.semilogx(
+            rxs / L_f, get_F('uy') / get_F('ux'), color_list[run_nb],
+            linewidth=1)
 
-def fig12_atm(fig, ax):
-    ax.set_xlabel('$r$ (km)')
-    ax.set_ylabel('$F_T, F_L$')
-    ax.hold(True)
-    ax.set_xscale('log')
-    ax.set_yscale('log')
-    ax.set_xlim([1, 2e3])
-    ax.set_ylim([2, 500])
-    # ax_inset = _ax_inset(fig, '$r$', 0.75 + 0.01)
+    # ax.legend(fontsize=fontsize, loc='lower left')
+
+    
+def plot_df(df, fig, ax, ax_inset):     
+    ax_a, ax_b = ax
+    test_mode = False
+    for run_nb, (idx, row) in enumerate(df.iterrows()):
+        short_name = row["short name"]
+        tmin = row["$t_{stat}$"]
+        fig12_flatness(
+            paths_sim[short_name], fig, ax_a, tmin=tmin, run_nb=run_nb,
+            key_var=('ux',), cache=test_mode
+        )
+        fig12_flatness(
+            paths_sim[short_name], fig, ax_b, tmin=tmin, run_nb=run_nb,
+            key_var=('uy',), cache=test_mode, ax_inset=ax_inset
+        )
+        if test_mode:
+            break
+        # fig12_atm(fig, ax_b)
 
 
 if __name__ == '__main__':
     matplotlib_rc(fontsize=fontsize)
     path_fig = exit_if_figure_exists(__file__)
-    set_figsize(5.12, 2.)
-    fig, ax = pl.subplots(ncols=2)
-    ax_a, ax_b = ax.ravel()
-    fig.tight_layout(pad=2)
-    fig12_flatness(paths_sim['noise_c20nh7680Buinf'], fig, ax_a, tmin=20)
-    fig12_atm(fig, ax_b)
+    set_figsize(5, 4)
+    fig, ax = pl.subplots(2, 2, sharey=True, sharex=True)
+
+    df_w = load_df("df_w")
+    df_3840 = df_w[df_w["$n$"] == 3840]
+    df_7680 = df_w[df_w["$n$"] == 7680]
+    
+    ax_inset3 = _ax_inset(fig, '$r_x/L_f$', 0.325, 0.362)
+    ax_inset7 = _ax_inset(fig, '$r_x/L_f$', 0.775, 0.362)
+
+    plot_df(df_3840, fig, ax[:,0], ax_inset3)
+    plot_df(df_7680, fig, ax[:,1], ax_inset7)
+
+    ax[0,0].set_ylabel('$F_L$')
+    ax[1,0].set_ylabel('$F_T$')
+    ax[0,0].annotate(
+        "increasing $c$",
+        (1e-2, 3e0),
+        (2e-1, 1e1), 
+        "data",
+        arrowprops={"arrowstyle": "simple"})
+    fig.tight_layout()
     pl.savefig(path_fig)
+    pl.savefig(path_fig.replace(".png", ".pdf"))
