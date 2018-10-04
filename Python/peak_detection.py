181c3bc0c260b405816fbb3c111248fe682ac4f9
Ashwin Vishnu <avmo@kth.se>
2018-10-04 14:04:25 +0200

    Peak detection in Beskow

--- Python/peak_detection.py
+++ Python/peak_detection.py
@@ -16,18 +16,18 @@ def detect_shocks(sim, i0=None, i1=None, debug=False, **kwargs):
         raise ValueError
 
     div1d_orig = div1d
-        
+
     # filter
-    height = (div1d.min(), np.median(div1d))
+    # height = (div1d.min(), np.median(div1d))
     # peaks, _ = find_peaks(div1d, height=height, **kwargs)
     peaks = indexes(-div1d, thres=0.5, min_dist=3)
     if debug:
-        print("Height =",height)
+        print("Height =", height)
         print(f"Detected {len(peaks)} peaks")
         plt.figure(dpi=150)
         x = sim.oper.x_seq
-        plt.plot(x, div1d_orig, 'k', label="original div")
-        # plt.plot(div1d, 'g--', label="preprocessed div")    
+        plt.plot(x, div1d_orig, "k", label="original div")
+        # plt.plot(div1d, 'g--', label="preprocessed div")
         plt.plot(x[peaks], div1d[peaks], "x")
         plt.xlabel("$x$")
         plt.ylabel(r"$\nabla.\bf u$")
@@ -45,7 +45,7 @@ def avg_shock_seperation_1d(sim, i0=None, i1=None):
 
 def avg_shock_seperation(sim, num_samples=200, averaged=True, ci=0.95):
     ds = []
-    for i in np.linspace(0, sim.oper.nx_seq-1, num_samples, dtype=int):
+    for i in np.linspace(0, sim.oper.nx_seq - 1, num_samples, dtype=int):
         ds0 = avg_shock_seperation_1d(sim, i0=i)
         ds1 = avg_shock_seperation_1d(sim, i1=i)
         ds.extend([ds0, ds1])
@@ -55,8 +55,9 @@ def avg_shock_seperation(sim, num_samples=200, averaged=True, ci=0.95):
         return ds
 
 
-def avg_shock_seperation_from_shortname(short_name, save_as="shock_sep.csv",
-                                        dict_paths=paths_sim):
+def avg_shock_seperation_from_shortname(
+    short_name, save_as="dataframes/shock_sep.csv", dict_paths=paths_sim
+):
     path = dict_paths[short_name]
     sim = fls.load_state_phys_file(path, merge_missing_params=True)
     mean, std = avg_shock_seperation(sim)
@@ -64,11 +65,12 @@ def avg_shock_seperation_from_shortname(short_name, save_as="shock_sep.csv",
         with open(save_as, "a") as f:
             result = "# short_name,\t path,\t mean,\t std\n"
             f.write(result)
-        
+
     with open(save_as, "a") as f:
         result = f"{short_name},\t {path},\t {mean}, \t {std}\n"
         f.write(result)
     return mean, std
+
 
 def run(nh_min, nh_max=10_000):
     df_all = load_df()
@@ -76,11 +78,11 @@ def run(nh_min, nh_max=10_000):
     for i in range(len(df)):
         short = df.iloc[i]["short name"]
         print(paths_sim[short])
-    
+
     return (
-        df["short name"].apply(
-            avg_shock_seperation_from_shortname).apply(
-                pd.Series)
+        df["short name"]
+        .apply(avg_shock_seperation_from_shortname)
+        .apply(pd.Series)
     )
 
 
@@ -88,5 +90,7 @@ if __name__ == "__main__":
     for dict_paths in (paths_sim, paths_sim_old):
         for short in dict_paths:
             print(short)
-            avg_shock_seperation_from_shortname(short, dict_paths=dict_paths)
+            mean, std = avg_shock_seperation_from_shortname(
+                short, dict_paths=dict_paths
+            )
             print(f"mean = {mean}, std = {std}")
